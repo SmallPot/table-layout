e8d19867ceaf5c8a0092a7a2f9f93df945b7dc44
small_pot <1198334022@qq.com>
2023-01-31 17:46:28 +0800

    初始文件

--- README.md
+++ README.md
@@ -1,1 +1,95 @@
-# table-layout
+## Install
+
+```
+npm install @smallpot/table-layout
+```
+
+## opts Attributes
+
+| 参数 | 说明 | 类型 | 是否必传 | 默认值 |
+|-|-|-|-|-|
+| xDrag | 是否开启横向拖拽 | Boolean | 可选 | true |
+| yDrag | 是否开启纵向拖拽 | Boolean | 可选 | true |
+| rowHeight | 默认行高 | Number | 可选 | 32 |
+| columnWidth | 默认列宽 | Number | 可选 | 100 |
+| tableProps | table标签配置项(props) | Object | 可选 | { className:[],style:{},attr:{} } |
+| tbodyProps | tbody标签配置项(props) | Object | 可选 | { className:[],style:{},attr:{} } |
+| trProps | tr标签配置项(props) | Object | 可选 | { className:[],style:{},attr:{} } |
+| tdProps | td标签配置项(props) | Object | 可选 | { className:[],style:{},attr:{} } |
+
+
+
+## 标签配置项(props) 
+
+| 字段 | 说明 | 类型 | 是否必传 | 默认值 |
+|-|-|-|-|-|
+| className | 标签类名，eg：["aClassName","bClassName"] | Array | 可选 | [] |
+| style | 标签内联样式，eg：{ color:"red",backgroundColor:"green" } | Object | 可选 | {} |
+| attr | 标签自定义属性，eg：{ isOpen:true } | Object | 可选 | {} |
+
+
+## Events
+
+| 方法名 | 说明 | 参数 | 是否必传 | 默认值 |
+|-|-|-|-|-|
+| active | 对象初始化，启用事件监听 | - | - | - |
+| mergeTdFn | 合并选中单元格 | - | - | - |
+| disassemblyFn | 拆解选中单元格 | - | - | - |
+| delEntireColumn | 删除选中单元格整列 | - | - | - |
+| delEntireRow | 删除选中单元格整行 | - | - | - |
+| insertColumnBefore | 选中单元格左侧插入列 | Number | 可选 | 1 |
+| insertColumnAfter | 选中单元格右侧插入列 | Number | 可选 | 1 |
+| insertRowAhead | 在选中单元格前方插入行 | Number | 可选 | 1 |
+| insertRowBehind | 在选中单元格后方插入行 | Number | 可选 | 1 |
+| cancelAllSelected | 取消选中单元格 | - | - | - |
+| setOpts | 设置opts | Object | 必填 | {} |
+| setSelectAreaStyle | 设置选择区域样式 | Object | 可选 | { width:"",height:"",boxSizing:"border-box",backgroundColor:"rgba(0, 0, 0, 0.1)",border:"2px solid rgb(31, 187, 125)",position: "absolute",left:"",top:"",pointerEvents:"none"} |
+| getSelectArr | 获取选中单元格 | - | - | - |
+
+
+
+## Example
+
+```
+<div class="menu">
+     <button type="button" id="mergeTd" >合并</button>
+     <button type="button" id="insertColumnAfter">右侧插入列</button>
+</div>
+
+<table id="tableBox">
+  <tr>
+    <td>单元格</td>
+    <td>单元格</td>
+    <td>单元格</td>
+    <td>单元格</td>
+  </tr>
+  <tr>
+    <td>单元格</td>
+    <td>单元格</td>
+    <td>单元格</td>
+    <td>单元格</td>
+  </tr>
+</table>
+
+
+<script type="module">
+  import TableLayout from "@smallpot/table-layout/lib"
+
+  let obj=new TableLayout(document.getElementById("tableBox"),{
+    yDrag:false,//是否开启纵向拖拽
+    tableProps:{
+      className:[],
+      style:{},
+      attr:{}
+    }
+  });
+
+   document.getElementById("mergeTd").addEventListener('click',e=>{
+      obj.mergeTdFn();
+   })
+   document.getElementById("insertColumnAfter").addEventListener('click',e=>{
+      obj.insertColumnAfter(1);
+   })
+
+</script>
+```
---
+++ src/App.vue
@@ -0,0 +1,535 @@
+<template>
+  <div id="app">
+    <div class="menu">
+       <button type="button" id="mergeTd">合并</button>
+			 <button type="button" id="disassemblyFn">拆解</button>
+			 <button type="button" id="clearSelect">清除选中</button>
+			 <button type="button" id="delEntireColumn">删除整列</button>
+			 <button type="button" id="delEntireRow">删除整行</button>
+			 <button type="button" id="insertColumnBefore">左侧插入列</button>
+			 <button type="button" id="insertColumnAfter">右侧插入列</button>
+			 <button type="button" id="insertRowAhead">在前方插入行</button>
+			 <button type="button" id="insertRowBehind">在后方插入行</button>
+    </div>
+    <div class="container">
+      <table id="tableBox">
+        <tr>
+            <td>0</td>
+            <td>1</td>
+            <td>2</td>
+            <td>3</td>
+            <td>4</td>
+            <td>5</td>
+            <td>6</td>
+            <td>7</td>
+            <td>8</td>
+            <td>9</td>
+            <td>10</td>
+            <td>11</td>
+            <td rowspan="2">12</td>
+            <td>13</td>
+            <td colspan="2">14</td>
+        </tr>
+        <tr >
+            <td>1</td>
+            <td>亚瑟1</td>
+            <td>亚瑟1</td>
+            <td>亚瑟1</td>
+            <td>亚瑟1</td>
+            <td>亚瑟1</td>
+            <td>亚瑟1</td>
+            <td>亚瑟1</td>
+            <td>亚瑟1</td>
+            <td>亚瑟1</td>
+            <td>亚瑟1</td>
+            <td>亚瑟1</td>
+            <td rowspan="2">亚瑟1</td>
+            <td colspan="2" rowspan="2">亚瑟1</td>
+        </tr>
+        <tr >
+            <td>2</td>
+            <td>亚瑟2</td>
+            <td>亚瑟2</td>
+            <td>亚瑟2</td>
+            <td>亚瑟2</td>
+            <td>亚瑟2</td>
+            <td>亚瑟2</td>
+            <td>亚瑟2</td>
+            <td>亚瑟2</td>
+            <td>亚瑟2</td>
+            <td>亚瑟2</td>
+            <td>亚瑟2</td>
+            <td>亚瑟2</td>
+        </tr>
+        <tr >
+            <td>3</td>
+            <td rowspan="3">亚瑟3</td>
+            <td colspan="2" rowspan="4">亚瑟3....</td>
+            <td>亚瑟3</td>
+            <td colspan="4">亚瑟3</td>
+            <td>亚瑟3</td>
+            <td>亚瑟3</td>
+            <td>亚瑟3</td>
+            <td>亚瑟3</td>
+            <td>亚瑟3</td>
+            <td>亚瑟3</td>
+            <td>亚瑟3</td>
+        </tr>
+        <tr >
+            <td>4</td>
+            <td>亚瑟4</td>
+            <td>亚瑟4</td>
+            <td>亚瑟4</td>
+            <td>亚瑟4</td>
+            <td>亚瑟4</td>
+            <td>亚瑟4</td>
+            <td>亚瑟4</td>
+            <td>亚瑟4</td>
+            <td>亚瑟4</td>
+            <td>亚瑟4</td>
+            <td>亚瑟4</td>
+            <td>亚瑟4</td>
+         
+        </tr>
+        <tr >
+            <td>5</td>
+            <td>亚瑟5</td>
+            <td>亚瑟5</td>
+            <td>亚瑟5</td>
+            <td>亚瑟5</td>
+            <td>亚瑟5</td>
+            <td>亚瑟5</td>
+            <td>亚瑟5</td>
+            <td>亚瑟5</td>
+            <td>亚瑟5</td>
+            <td>亚瑟5</td>
+            <td>亚瑟5</td>
+            <td>亚瑟5</td>
+    
+        </tr>
+        <tr >
+            <td>6</td>
+            <td>亚瑟6</td>
+            <td>亚瑟6</td>
+            <td>亚瑟6</td>
+            <td>亚瑟6</td>
+            <td>亚瑟6</td>
+            <td>亚瑟6</td>
+            <td>亚瑟6</td>
+            <td>亚瑟6</td>
+            <td>亚瑟6</td>
+            <td>亚瑟6</td>
+            <td>亚瑟6</td>
+            <td>亚瑟6</td>
+            <td>亚瑟6</td>
+        </tr>
+        <tr >
+            <td>7</td>
+            <td>亚瑟7</td>
+            <td>亚瑟7</td>
+            <td colspan="4" rowspan="4">亚瑟7</td>
+            <td>亚瑟7</td>
+            <td>亚瑟7</td>
+            <td>亚瑟7</td>
+            <td>亚瑟7</td>
+            <td>亚瑟7</td>
+            <td>亚瑟7</td>
+            <td>亚瑟7</td>
+            <td>亚瑟7</td>
+            <td>亚瑟7</td>
+        </tr>
+        <tr >
+            <td>8</td>
+            <td>亚瑟8</td>
+            <td>亚瑟8</td>
+            <td>亚瑟8</td>
+            <td>亚瑟8</td>
+            <td colspan="4" rowspan="2">亚瑟8</td>
+            <td>亚瑟8</td>
+            <td>亚瑟8</td>
+            <td>亚瑟8</td>
+        </tr>
+        <tr >
+            <td>9</td>
+            <td>亚瑟9</td>
+            <td>亚瑟9</td>
+            <td>亚瑟9</td>
+            <td>亚瑟9</td>
+            <td>亚瑟9</td>
+            <td>亚瑟9</td>
+            <td>亚瑟9</td>
+       
+
+        </tr>
+        <tr >
+            <td>10</td>
+            <td>亚瑟10</td>
+            <td>亚瑟10</td>
+            <td>亚瑟10</td>
+            <td colspan="3" rowspan="4">亚瑟10***</td>
+            <td>亚瑟10ss</td>
+            <td>亚瑟10dd</td>
+            <td>亚瑟10</td>
+            <td>亚瑟10</td>
+            <td>亚瑟10</td>
+        </tr>
+        <tr >
+            <td>11</td>
+            <td>亚瑟11</td>
+            <td>亚瑟11</td>
+            <td>亚瑟11</td>
+            <td>亚瑟11</td>
+            <td>亚瑟11</td>
+            <td>亚瑟11</td>
+            <td>亚瑟11</td>
+            <td>亚瑟11</td>
+            <td>亚瑟11</td>
+            <td>亚瑟11</td>
+            <td>亚瑟11</td>
+            <td>亚瑟11</td>
+        </tr>
+        <tr >
+            <td>12</td>
+            <td>亚瑟12</td>
+            <td>亚瑟12</td>
+            <td>亚瑟12</td>
+            <td>亚瑟12</td>
+            <td>亚瑟12</td>
+            <td>亚瑟12</td>
+            <td>亚瑟12</td>
+            <td>亚瑟12</td>
+            <td>亚瑟12</td>
+            <td>亚瑟12</td>
+            <td>亚瑟12</td>
+            <td>亚瑟12</td>
+        </tr>
+        <tr >
+            <td>13</td>
+            <td>亚瑟13</td>
+            <td>亚瑟13</td>
+            <td>亚瑟13</td>
+            <td>亚瑟13</td>
+            <td>亚瑟13</td>
+            <td>亚瑟13</td>
+            <td rowSpan="4">亚瑟13pp</td>
+            <td>亚瑟13</td>
+            <td>亚瑟13</td>
+            <td>亚瑟13</td>
+            <td>亚瑟13</td>
+            <td>亚瑟13</td>
+        </tr>
+        <tr >
+            <td>14</td>
+            <td>亚瑟14</td>
+            <td>亚瑟14</td>
+            <td>亚瑟14</td>
+            <td>亚瑟14</td>
+            <td>亚瑟14</td>
+            <td>亚瑟14</td>
+            <td>亚瑟14</td>
+            <td>亚瑟14</td>
+            <td>亚瑟14</td>
+            <td>亚瑟14</td>
+            <td>亚瑟14</td>
+            <td>亚瑟14</td>
+            <td>亚瑟14</td>
+            <td>亚瑟14</td>
+        </tr>
+        <tr >
+            <td>15</td>
+            <td>亚瑟15</td>
+            <td>亚瑟15</td>
+            <td>亚瑟15</td>
+            <td>亚瑟15</td>
+            <td>亚瑟15</td>
+            <td>亚瑟15</td>
+            <td>亚瑟15</td>
+            <td>亚瑟15</td>
+            <td>亚瑟15</td>
+            <td>亚瑟15</td>
+            <td>亚瑟15</td>
+            <td>亚瑟15</td>
+            <td>亚瑟15</td>
+            <td>亚瑟15</td>
+        </tr>
+        <tr >
+            <td>16</td>
+            <td>亚瑟16</td>
+            <td>亚瑟16</td>
+            <td>亚瑟16</td>
+            <td>亚瑟16</td>
+            <td>亚瑟16</td>
+            <td>亚瑟16</td>
+            <td>亚瑟16</td>
+            <td>亚瑟16</td>
+            <td>亚瑟16</td>
+            <td>亚瑟16</td>
+            <td>亚瑟16</td>
+            <td>亚瑟16</td>
+            <td>亚瑟16</td>
+            <td>亚瑟16</td>
+        </tr>
+        <tr >
+            <td>17</td>
+            <td>亚瑟17</td>
+            <td>亚瑟17</td>
+            <td>亚瑟17</td>
+            <td>亚瑟17</td>
+            <td>亚瑟17</td>
+            <td>亚瑟17</td>
+            <td>亚瑟17</td>
+            <td>亚瑟17</td>
+            <td>亚瑟17</td>
+            <td>亚瑟17</td>
+            <td>亚瑟17</td>
+            <td>亚瑟17</td>
+            <td>亚瑟17</td>
+            <td>亚瑟17</td>
+            <td>亚瑟17</td>
+        </tr>
+        <tr >
+            <td>18</td>
+            <td>亚瑟18</td>
+            <td>亚瑟18</td>
+            <td>亚瑟18</td>
+            <td>亚瑟18</td>
+            <td>亚瑟18</td>
+            <td>亚瑟18</td>
+            <td>亚瑟18</td>
+            <td>亚瑟18</td>
+            <td>亚瑟18</td>
+            <td>亚瑟18</td>
+            <td>亚瑟18</td>
+            <td>亚瑟18</td>
+            <td>亚瑟18</td>
+            <td>亚瑟18</td>
+            <td>亚瑟18</td>
+        </tr>
+        <tr >
+            <td colspan="16">19</td>
+        </tr>
+				<tr >
+				    <td colspan="16" rowspan="2">***********</td>
+				</tr>
+				<tr >
+					<!-- <td>亚瑟v</td> -->
+				</tr>
+        <tr >
+            <td>20</td>
+            <td>亚瑟20</td>
+            <td>亚瑟20</td>
+            <td>亚瑟20</td>
+            <td>亚瑟20</td>
+            <td>亚瑟20</td>
+            <td>亚瑟20</td>
+            <td>亚瑟20</td>
+            <td>亚瑟20</td>
+            <td>亚瑟20</td>
+            <td>亚瑟20</td>
+            <td>亚瑟20</td>
+            <td>亚瑟20</td>
+            <td>亚瑟20</td>
+            <td>亚瑟20</td>
+            <td>亚瑟20</td>
+        </tr>
+        <tr >
+            <td>21</td>
+            <td>亚瑟21</td>
+            <td>亚瑟21</td>
+            <td>亚瑟21</td>
+            <td>亚瑟21</td>
+            <td>亚瑟21</td>
+            <td>亚瑟21</td>
+            <td>亚瑟21</td>
+            <td>亚瑟21</td>
+            <td>亚瑟21</td>
+            <td>亚瑟21</td>
+            <td>亚瑟21</td>
+            <td>亚瑟21</td>
+            <td>亚瑟21</td>
+            <td>亚瑟21</td>
+            <td>亚瑟21</td>
+        </tr>
+        <tr >
+            <td colspan="2" rowspan="2">22</td>
+            <td>亚瑟22</td>
+            <td>亚瑟22</td>
+            <td>亚瑟22</td>
+            <td>亚瑟22</td>
+            <td>亚瑟22</td>
+            <td>亚瑟22</td>
+            <td>亚瑟22</td>
+            <td>亚瑟22</td>
+            <td>亚瑟22</td>
+            <td>亚瑟22</td>
+            <td>亚瑟22</td>
+            <td>亚瑟22</td>
+            <td>亚瑟22</td>
+            <td>亚瑟22</td>
+        </tr>
+        <tr >
+            <td>亚瑟23b</td>
+            <td>亚瑟23</td>
+            <td>亚瑟23</td>
+            <td>亚瑟23</td>
+            <td>亚瑟23</td>
+            <td>亚瑟23</td>
+            <td>亚瑟23</td>
+            <td>亚瑟23</td>
+            <td>亚瑟23</td>
+            <td>亚瑟23</td>
+            <td>亚瑟23</td>
+            <td>亚瑟23</td>
+            <td>亚瑟23</td>
+            <td>亚瑟23</td>
+        </tr>
+        <tr >
+            <td>24</td>
+            <td>亚瑟24</td>
+            <td>亚瑟24</td>
+            <td>亚瑟24</td>
+            <td>亚瑟24</td>
+            <td>亚瑟24</td>
+            <td>亚瑟24</td>
+            <td>亚瑟24</td>
+            <td>亚瑟24</td>
+            <td>亚瑟24</td>
+            <td>亚瑟24</td>
+            <td>亚瑟24</td>
+            <td>亚瑟24</td>
+            <td>亚瑟24</td>
+            <td>亚瑟24</td>
+            <td>亚瑟24</td>
+        </tr>
+        <tr >
+            <td>25</td>
+            <td>亚瑟25</td>
+            <td>亚瑟25</td>
+            <td>亚瑟25</td>
+            <td>亚瑟25</td>
+            <td>亚瑟25</td>
+            <td>亚瑟25</td>
+            <td>亚瑟25</td>
+            <td>亚瑟25</td>
+            <td>亚瑟25</td>
+            <td>亚瑟25</td>
+            <td>亚瑟25</td>
+            <td>亚瑟25</td>
+            <td>亚瑟25</td>
+            <td>亚瑟25</td>
+            <td rowspan="2">亚瑟25</td>
+        </tr>
+        <tr >
+            <td>26</td>
+            <td>亚瑟26</td>
+            <td>亚瑟26</td>
+            <td>亚瑟26</td>
+            <td>亚瑟26</td>
+            <td>亚瑟26</td>
+            <td>亚瑟26</td>
+            <td>亚瑟26</td>
+            <td>亚瑟26</td>
+            <td>亚瑟26</td>
+            <td>亚瑟26</td>
+            <td colspan="2">亚瑟26</td>
+            <td>亚瑟26</td>
+            <td>亚瑟26</td>
+        </tr>
+    </table>
+    </div>
+  </div>
+</template>
+
+<script>
+
+import  TableLayout from "../lib/index.js"
+export default {
+  name: 'App',
+  components: {
+  },
+  data(){
+    return{
+   
+    }
+  },
+  mounted(){
+    let obj=new  TableLayout(document.getElementById("tableBox"),{
+		 rowHeight:60,
+		 columnWidth:200
+	 });
+	  obj.active();
+    
+    document.getElementById("mergeTd").addEventListener('click',()=>{
+      obj.mergeTdFn();
+    })
+    document.getElementById("disassemblyFn").addEventListener('click',()=>{
+        obj.disassemblyFn();
+    })
+    document.getElementById("clearSelect").addEventListener('click',()=>{
+        obj.cancelAllSelected();
+    })
+    document.getElementById("delEntireColumn").addEventListener('click',()=>{
+        obj.delEntireColumn();
+    })
+    document.getElementById("delEntireRow").addEventListener('click',()=>{
+        obj.delEntireRow();
+    })
+    document.getElementById("insertColumnAfter").addEventListener('click',()=>{
+        obj.insertColumnAfter();
+    })
+    document.getElementById("insertColumnBefore").addEventListener('click',()=>{
+        obj.insertColumnBefore();
+    })
+    document.getElementById("insertRowBehind").addEventListener('click',()=>{
+        obj.insertRowBehind();
+    })
+    document.getElementById("insertRowAhead").addEventListener('click',()=>{
+        obj.insertRowAhead();
+    })
+ 
+
+  }
+}
+</script>
+
+<style>
+*{
+    margin: 0;
+    padding: 0;
+}
+body {
+    position: relative;
+}
+table {
+    border-collapse: collapse;
+    position: relative;
+    left: 0;
+    top: 0;
+    table-layout:fixed ;
+}
+td {
+    border: 1px solid #d4d4d4;
+    padding: 5px 0px;
+    text-align: center;
+    /* cursor: default; */
+    color: #000;
+    width: 100px;
+    height: 20px;
+
+}
+.menu{
+  position: fixed;
+  left: 0;
+  top: 0;
+  padding: 10px
+}
+.menu>button {
+  margin-right: 10px;
+}
+.container{
+  height: 700px;
+  overflow: auto;
+  margin-top: 50px;
+  border: 1px solid #000;
+}
+</style>
